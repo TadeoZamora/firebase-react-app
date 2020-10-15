a607307f7788a34d55d4a42e40a03b23c80c6c25
TadeoZamora <zamora.figueroa@gmail.com>
2020-10-15 14:55:48 -0700

    Add Context to main component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import AuthLayout from './layouts/Auth';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import UserContext from './context/UserContext';
+import AdminLayout from './layouts/Admin';
 
 const App = () =>{
-  return (<BrowserRouter>
-    <Switch>
-      <Route path="/auth" render={props => <AuthLayout {...props} />} />
-    </Switch>
-  </BrowserRouter>)
+  return (
+    <UserContext>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/auth" render={props => <AuthLayout {...props} />} />
+          <Route path="/admin" render={props => <AdminLayout {...props} />} />
+          <Redirect from="/" to="/admin/home" />
+        </Switch>
+      </BrowserRouter>
+    </UserContext>
+  )
 }
 
 export default App;
