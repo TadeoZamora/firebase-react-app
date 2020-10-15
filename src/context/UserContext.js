ddaf4756d96cc634bcc65f99e1b2c89ae1e3434a
TadeoZamora <zamora.figueroa@gmail.com>
2020-10-15 14:56:58 -0700

    fix problem with exporting the context

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,6 +1,6 @@
 import React,{ createContext,useState } from 'react';
 
-export default User = createContext({
+export const User = createContext({
     email : '',
     name : '',
     isAuthenticated : false
