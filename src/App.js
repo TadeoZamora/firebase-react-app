3fb6e407a1d9a4b4ba2951767dd0fddf23c2df57
TadeoZamora <zamora.figueroa@gmail.com>
2020-10-15 10:22:04 -0700

    Add simple route to auth layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import AuthLayout from './layouts/Auth';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () =>{
+  return (<BrowserRouter>
+    <Switch>
+      <Route path="/auth" render={props => <AuthLayout {...props} />} />
+    </Switch>
+  </BrowserRouter>)
 }
 
 export default App;
