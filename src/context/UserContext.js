52f00166267c7cdb9a86ed4f8bba130d0311bdbe
TadeoZamora <zamora.figueroa@gmail.com>
2020-10-16 12:00:34 -0700

    Add validations of user session

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,5 +1,5 @@
-import React,{ createContext,useState } from 'react';
-
+import React,{ createContext,useState, useEffect } from 'react';
+import firebase from '../services/Firebase'
 export const User = createContext({
     email : '',
     name : '',
@@ -13,16 +13,43 @@ const UserContext = ({...props}) => {
         name : '',
         isAuthenticated : false
     })
+    const [loading, setLoading] = useState(true);
     const SetUserState = (key,value) => {
         setUser({
             ...user, [key] : value
         })
     }
+    const ValidateUserAccess = async ( ) => {
+        //this observable is listening avery change with the user authentication data
+        //if the user signout, this observable will return null
+        firebase.auth().onAuthStateChanged(userData => {
+            console.log('[User]',user.isAuthenticated)
+            if (userData !== null) {
+                setUser({
+                    ...user,  isAuthenticated : true
+                })
+            }
+            setLoading(false)
+        })
+        return
+    }
+    const LogOut = async ( )=>{
+        //signOut remove the user from firebase
+        await firebase.auth().signOut()
+        setLoading(false)
+        setUser({
+            ...user,  isAuthenticated : false
+        })
+    }
+    
     return (
         <User.Provider
             value={{
                 user : user,
-                SetUserState : SetUserState
+                SetUserState : SetUserState,
+                ValidateUserAccess : ValidateUserAccess,
+                LogOut : LogOut,
+                loading : loading
             }}
         >
             {
